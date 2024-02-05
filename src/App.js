732d980753fb9b721c095de69455868a1d90be66
drewschmidt145 <drewschmidt145@gmail.com>
2024-02-05 13:24:09 -0700

    Added warning alert for going offline
    when active, user should receive a alert when the page is loaded offline

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
-import { InfoAlert, ErrorAlert } from './components/Alert';
+import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
 
 import './App.css';
 
@@ -14,8 +14,14 @@ const App = () => {
   const [currentCity, setCurrentCity] = useState("See all cities");
   const [infoAlert, setInfoAlert] = useState("");
   const [errorAlert, setErrorAlert] = useState("");
+  const [warningAlert, setWarningAlert] = useState("");
 
   useEffect(() => {
+    if (navigator.onLine) {
+      setWarningAlert(" ");
+    } else {
+      setWarningAlert("You have gone offline, events may not be up to date!");
+    }
     fetchData();
   }, [currentCity, currentNOE]);
 
@@ -33,6 +39,7 @@ const App = () => {
       <div className='alerts-container'>
         {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
         {errorAlert.length ? <ErrorAlert text={errorAlert}/> : null}
+        {warningAlert.length ? <WarningAlert text={warningAlert}/> : null}
       </div>
       <CitySearch 
         allLocations={allLocations} 
--- src/components/Alert.js
+++ src/components/Alert.js
@@ -48,4 +48,12 @@ class ErrorAlert extends Alert {
     }
 }
 
-export { InfoAlert, ErrorAlert };
+class WarningAlert extends Alert {
+    constructor(props) {
+        super(props);
+        this.color = 'rgb(255, 0, 0)';
+        this.bgColor = 'rgb(220, 220, 255)';
+    }
+} 
+
+export { InfoAlert, ErrorAlert, WarningAlert };
