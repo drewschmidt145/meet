0cba1f691f1e3461775859581a0a7798adddf280
drewschmidt145 <drewschmidt145@gmail.com>
2024-02-14 12:16:27 -0700

    Update charts and tests failing because of charts

--- src/components/CityEventsCharts.js
+++ src/components/CityEventsCharts.js
@@ -1,14 +1,8 @@
+import { ScatterChart, Scatter, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 import { useState, useEffect } from 'react';
-import {
-  ScatterChart,
-  Scatter,
-  XAxis, YAxis,
-  CartesianGrid,
-  Tooltip,
-  ResponsiveContainer
-} from 'recharts';
 
 const CityEventsChart = ({ allLocations, events }) => {
+
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -18,33 +12,34 @@ const CityEventsChart = ({ allLocations, events }) => {
   const getData = () => {
     const data = allLocations.map((location) => {
       const count = events.filter((event) => event.location === location).length
-      const city = location.split(/, | - /)[0]
-      return { city, count };
+      const city = location.split((/, | - /))[0]
+      return { city, count }
     })
     return data;
   };
 
   return (
     <ResponsiveContainer width="99%" height={400}>
-    <ScatterChart
-      margin={{
-        top: 20,
-        right: 20,
-        bottom: 60,
-        left: -30,
-      }}
-    >
-      <CartesianGrid />
-      <XAxis 
-        type="category" dataKey="city" name="City" 
-        angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14 }}
-      />
-      <YAxis type="number" dataKey="count" name="Number of Events" />
-      <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-      <Scatter name="A school" data={data} fill="#8884d8" />
-    </ScatterChart>
+      <ScatterChart
+        margin={{
+          top: 20,
+          right: 20,
+          bottom: 60,
+          left: -30,
+        }}
+      >
+        <CartesianGrid fill="#a7e0d9" fillOpacity={0.4} />
+        <XAxis type="category" dataKey="city" name="City" angle={60} interval={0}
+          tick={{ dx: 20, dy: 40, fontSize: 14 }}
+        />
+        <YAxis type="number" dataKey="count" name="Number of events" allowDecimals={false} />
+        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+        <Scatter name="Events" data={data} fill="#ff0000" />
+      </ScatterChart>
     </ResponsiveContainer>
   );
 }
+
+
 
 export default CityEventsChart;
--- src/setupTests.js
+++ src/setupTests.js
@@ -21,3 +21,21 @@ console.error = (...args) => {
 }
 
 jest.setTimeout(50000);
+
+
+const { ResizeObserver } = window;
+
+beforeEach(() => {
+    //@ts-ignore
+    delete window.ResizeObserver;
+    window.ResizeObserver = jest.fn().mockImplementation(() => ({
+      observe: jest.fn(),
+      unobserve: jest.fn(),
+      disconnect: jest.fn(),
+    }));
+  });
+  
+  afterEach(() => {
+    window.ResizeObserver = ResizeObserver;
+    jest.restoreAllMocks();
+  });
