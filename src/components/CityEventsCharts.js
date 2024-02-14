830f0abc643bfa000439549bd73718acd676918f
drewschmidt145 <drewschmidt145@gmail.com>
2024-02-14 12:08:29 -0700

    Update CityEventsCharts.js

--- src/components/CityEventsCharts.js
+++ src/components/CityEventsCharts.js
@@ -18,7 +18,7 @@ const CityEventsChart = ({ allLocations, events }) => {
   const getData = () => {
     const data = allLocations.map((location) => {
       const count = events.filter((event) => event.location === location).length
-      const city = location.split(', ')[0]
+      const city = location.split(/, | - /)[0]
       return { city, count };
     })
     return data;
@@ -30,8 +30,8 @@ const CityEventsChart = ({ allLocations, events }) => {
       margin={{
         top: 20,
         right: 20,
-        bottom: 20,
-        left: 20,
+        bottom: 60,
+        left: -30,
       }}
     >
       <CartesianGrid />
